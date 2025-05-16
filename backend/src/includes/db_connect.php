0d81c261e37e6c2adb22cfeb4a86e9dd02df7c79
EmrisEvegheden <emrisevigheden@gmail.com>
2025-05-16 10:13:49 +0200

    create frontend

--- backend/src/includes/db_connect.php
+++ backend/src/includes/db_connect.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost"
-$username = "root"
-$password = ""
-$dbname = "lernprojekt"
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "lernprojekt";
 
 
 ?>
