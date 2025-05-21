89a743a8758b5f5c3e096d7e31234a306fbdc391
Gabriel Maurer <103388157+GabrielM-512@users.noreply.github.com>
2025-05-21 13:06:52 +0200

    Update db_connect.php

--- backend/src/includes/db_connect.php
+++ backend/src/includes/db_connect.php
@@ -8,11 +8,11 @@ $conn = new mysqli($server, $username, $password);
 
 if ($conn->connect_error) {
 	die("Connection error: " . $conn->connect_error);
-} else echo "conn success!<br>";
+}
 
 $sql = "USE $dbname;";
 
-if ($conn->query($sql) === TRUE) {
-	echo "USE schule;<br>";
-} else echo "error: " . conn->error;
+if ($conn->query($sql) !== TRUE) {
+	echo "error: " . conn->error;
+}
 ?>
