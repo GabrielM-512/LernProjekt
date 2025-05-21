53e497c2463f366942ff0321f062c0d34f949420
Gabriel Maurer <103388157+GabrielM-512@users.noreply.github.com>
2025-05-21 12:58:17 +0200

    Update db_connect.php
    Finished the include for connecting to the db

--- backend/src/includes/db_connect.php
+++ backend/src/includes/db_connect.php
@@ -4,5 +4,15 @@ $username = "root";
 $password = "";
 $dbname = "lernprojekt";
 
+$conn = new mysqli($server, $username, $password);
 
+if ($conn->connect_error) {
+	die("Connection error: " . $conn->connect_error);
+} else echo "conn success!<br>";
+
+$sql = "USE $dbname;";
+
+if ($conn->query($sql) === TRUE) {
+	echo "USE schule;<br>";
+} else echo "error: " . conn->error;
 ?>
