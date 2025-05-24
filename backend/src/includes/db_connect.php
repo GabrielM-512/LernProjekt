22f4d0a4465ea1f717d150b89a75ea2401028528
GabrielM-512 <gabriel.maurer@gmx.de>
2025-05-24 18:54:18 +0200

    Change from defaulting to not connecting to the DB to defaulting to connect to the DB

--- backend/src/includes/db_connect.php
+++ backend/src/includes/db_connect.php
@@ -1,7 +1,7 @@
 <?php
 /*
 This file serves as the global include for connecting to the server. It should only ever be used as include_once or require_once.
-It provides the option to instantly connect to the database itself instead of just to the server by creating the variable $setDB and setting it to True prior to including this file.
+It provides the option to not instantly connect to the database itself and instead just connect to the server by creating the variable $setDB and setting it to True prior to including this file.
 */
 
 // configure server and DB data
@@ -17,15 +17,19 @@ if ($conn->connect_error) {
 	die("Connection error: " . $conn->connect_error);
 }
 
-// check if $setDB has been created and set to true. If so, connect to the DB.
+// check if $setDB has been created and set to False. If so, exit the script.
 if (isset($setDB)) {
-	if ($setDB) {
-		$sql = "USE $dbname;";
-
-		if ($conn->query($sql) !== TRUE) {
-			echo "error: " . conn->error;
-		}
+	if (!$setDB) {
+		return;
 	}
 }
+
+// connect to the DB
+$sql = "USE $dbname;";
+
+if ($conn->query($sql) !== TRUE) {
+	echo "error: " . conn->error;
+}
+
 
 ?>
