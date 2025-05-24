6c068b18eb7f47c3cf0a473b156c19aa9ef69e9b
GabrielM-512 <gabriel.maurer@gmx.de>
2025-05-24 18:41:26 +0200

    Mounted prepend_global.php and first copy of prepend.php for userManagement as outlined in the Documentation file.

---
+++ backend/prepend_global.php
@@ -0,0 +1,8 @@
+<?php
+
+/*
+This defines all filepaths which are relevant to some script. It is dynamically found by a prepend.php mounted in each subdirectory which includes actual scripts.
+*/
+
+define("BACKEND_INCLUDES", __DIR__ . "\src\includes");
+?>
---
+++ backend/src/userManagement/prepend.php
@@ -0,0 +1,35 @@
+<?php
+/*
+This file traverses up the directory tree and tries to search for prepend_global.php (as outlined in the documentation document).
+If it fails to reach it, it will die with an error message.
+
+This file should (!!!) be identical in every subdirectory which has some kind of .php script, as it dynamically finds prepend_global.php and thus will find it as long as there it is mounted at /backend.
+
+TBD: Implement some kind of system that alerts site admins about an error within prepend.php, as failing to find prepend_global.php before reaching /backend (or, even worse, the root of the actual filesystem) would mean not being able to use
+the global /includes/db_connect.php to establish a connection with the DB and input an error into the software_errors table.
+*/
+
+$dir = __DIR__;
+
+while (!file_exists($dir . '/prepend_global.php')) {
+    // check if /backend has already been reached. This check can be put here since, if backend has been reached and /prepend_global.php is mounted there, the loop won't be executed again.
+    // If it isn't mounted there, the loop will re-execute and die because it isn't authorised to go further than /backend.
+    if (basename($dir) === 'backend') {
+        die('Error: prepend_global.php not found in file system.');
+    }
+
+    // Get the parent directory of the current directory.
+    $parent = dirname($dir);
+
+    // Check if the root of the filesystem has been reached. The check must be made since if prepend.php is placed outside of /backend and its subdirectories,
+    // it won't ever fail upon reaching /backend and continue to the root of the filesystem, where it will be stuck in an infinite loop since trying to get the dirname(filesystem_root) == filesystem_root, and it will simply retry.
+    if ($parent === $dir) {
+        die('Error: Reached filesystem root without finding prepend_global.php.');
+    }
+    // overwrite the current directory with its parent directory and restart the loop
+    $dir = $parent;
+}
+
+// finally, include prepend_global.php to make is accessible to the calling script
+require_once($dir . '/prepend_global.php');
+?>
